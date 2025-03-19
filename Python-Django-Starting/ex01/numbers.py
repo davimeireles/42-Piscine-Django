b6c251e234af65dee0c08efff575bf8ed8abe446
Davi <davimeirelespn@gmail.com>
2025-03-19 18:09:58 +0000

    numbers.py working and printing all numbers from numbers.txt file line by line without commas.

---
+++ Python-Django-Starting/ex01/numbers.py
@@ -0,0 +1,21 @@
+def display_numbers_from_file():
+    
+    # Open the file in read mode
+    file = open("numbers.txt", "r")
+    
+    # Read the content of the file
+    numbers = file.read()
+    
+    # Split the content of the file into lines
+    lines = numbers.split(',')
+    
+    # Print the content of the file
+    for lines in lines:
+        print(lines)
+    
+    # Close the file
+    file.close()
+
+if __name__ == '__main__':
+    display_numbers_from_file()
+    
