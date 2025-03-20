2b9d36e7f00be9b19d51b454af5e35f6b545bd05
Davi Nunes <davimeirelespn@gmail.com>
2025-03-20 14:50:06 +0000

    Code documentation for all_in.py

--- Python-Django-Starting/ex05/all_in.py
+++ Python-Django-Starting/ex05/all_in.py
@@ -3,6 +3,15 @@ import sys
 
 def all_in(string):
     
+    """
+    This function takes a string of comma-separated values and determines if each value is a state or a capital city.
+    It prints the corresponding capital city and state if found, otherwise it indicates that the value is neither.
+
+    Args:
+    string (str): A string of comma-separated values representing state names or capital cities.
+    """
+    
+    # Dictionary mapping state names to their abbreviations
     states = {
     "Oregon" : "OR",
     "Alabama" : "AL",
@@ -10,6 +19,7 @@ def all_in(string):
     "Colorado" : "CO"
     }
     
+    # Dictionary mapping state abbreviations to their capital cities
     capital_cities = {
     "OR": "Salem",
     "AL": "Montgomery",
@@ -17,6 +27,7 @@ def all_in(string):
     "CO": "Denver"
     }
     
+    # Split the input string by commas to get individual values
     args = string.split(',')
         
     for line in args:
@@ -25,6 +36,7 @@ def all_in(string):
         lower_x = x.casefold()
 
         if len(x) > 0:
+            # Check if the value is a state name
             for states_key, states_value in states.items():
                 lower_key = states_key.casefold()
                 if lower_x == lower_key:
@@ -33,6 +45,7 @@ def all_in(string):
                             print(f"{capital_value} is the capital of {states_key}")              
                             found = True
 
+            # Check if the value is a capital city
             for capital_key, capital_value in capital_cities.items():
                 lower_key = capital_value.casefold()
                 if lower_x == lower_key:
@@ -41,6 +54,7 @@ def all_in(string):
                             print(f"{capital_value} is the capital of {states_key}")
                             found = True
 
+            # If the value is neither a state nor a capital city
             if not found:
                 print(f"{x} is neither a capital city nor a state")
 
