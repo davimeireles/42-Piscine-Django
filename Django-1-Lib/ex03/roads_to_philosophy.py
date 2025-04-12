563fe0e80f024626f8263dab9c3e80e921397ae8
Davi Nunes <davimeirelespn@gmail.com>
2025-04-12 13:47:01 +0000

    Road to philosophy using beautifulsoup and requests for wikipedia en url

--- Django-1-Lib/ex03/roads_to_philosophy.py
+++ Django-1-Lib/ex03/roads_to_philosophy.py
@@ -2,13 +2,85 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+"""
+Function to search inside the wikipedia website
+Trying to find the Philosophy article
+"""
+
 def road_to_pilho(argument):
 
-    wikipedia_url = 'https://pt.wikipedia.org/'
-    
-    result = requests.get(wikipedia_url)
-    doc = BeautifulSoup(result.text, 'html.parser')
-    print(doc.prettify)    
+    # Replace space for _ to search in the url
+    search_argument=argument.replace(' ', '_')
+
+    # Track visited pages to prevent loop
+    visited_pages = []
+
+    # Init page title null
+    page_title = ''
+
+    while page_title != 'Philosophy':
+
+        # Check if we  already visited the current page
+        if search_argument in visited_pages:
+            print('It leads to an infinite loop !')
+            break
+
+        # Append the current page to visited pages
+        visited_pages.append(search_argument)
+
+        # Mix default en url with the argument to search
+        wikipedia_url = f'https://en.wikipedia.org/wiki/{search_argument}'
+
+        # Make a request and get the html page
+        result = requests.get(wikipedia_url)
+
+        # Use BeautifulSoup to parse the result into a html code
+        doc = BeautifulSoup(result.text, 'html.parser')
+
+        # Get the page title to print in terminal
+        page_title = doc.find(id='firstHeading').string
+
+        print(page_title)
+
+        # Get content from element id bodyContent
+        body_content = doc.find(id='bodyContent')
+
+        next_link = None
+
+        for p in body_content.find_all('p'):
+            # Skip empty paragraphs
+            if not p.text.strip():
+                continue
+                
+            # Find all links in this paragraph
+            links = p.find_all('a')
+            
+            # Find the first valid link
+            for link in links:
+                # Check if it's a valid wiki link (not a reference, not in parentheses, etc.)
+                if (link.get('href') and 
+                    link['href'].startswith('/wiki/') and 
+                    ':' not in link['href'] and
+                    not link.find_parent('span', class_='reference') and
+                    'redlink=1' not in link.get('href', '')):
+                    
+                    next_link = link
+                    break
+            
+            # If we found a valid link in this paragraph, stop checking paragraphs
+            if next_link:
+                break
+        
+        # If no valid link was found
+        if not next_link:
+            print('It leads to a dead end !')
+            break
+
+        # Extract the next page to visit from the link
+        search_argument = next_link['href'].split('/wiki/')[1]
+
+    print(f'{len(visited_pages)} roads from {argument} to philosophy !')
+
 
 
 if __name__ == '__main__':
